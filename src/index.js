517e4e43ab722a52ad967a1760298e073015f1d4
jhonnycave <luis.j.cueva@gmail.com>
2022-04-21 11:55:09 -0500

    3:09:08

--- src/index.js
+++ src/index.js
@@ -7,12 +7,20 @@ import "./style/index.css";
 
 const arrayBooks = [
   {
+    id: 1,
     title: " Atomic Habits",
     author: " Amelia Hepworth",
     img: "https://images-na.ssl-images-amazon.com/images/I/51-uspgqWIL._SX329_BO1,204,203,200_.jpg",
   },
 
   {
+    id: 2,
+    title: " How to Win Friends & Influence People",
+    author: "Dale Carnegie",
+    img: "https://images-na.ssl-images-amazon.com/images/I/41C9YOYkkOL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+  },
+  {
+    id: 3,
     title: " How to Win Friends & Influence People",
     author: "Dale Carnegie",
     img: "https://images-na.ssl-images-amazon.com/images/I/41C9YOYkkOL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
@@ -22,27 +30,26 @@ const arrayBooks = [
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      ></Book>
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
+      {arrayBooks.map((book) => {
+        return <Book key={book.id} {...book} />;
+      })}
     </section>
   );
 }
 
 const Book = ({ img, title, author }) => {
+
+  /*  */
+  const clickHandler = (e) =>{
+    console.log(e);
+    console.log(e.target);
+    alert('hello from button')
+  }
+
+  const complexExample = (author)=>{
+    console.log(author);
+  }
+
   return (
     <article className="book">
       <img
@@ -53,8 +60,10 @@ const Book = ({ img, title, author }) => {
         src={img}
         alt=""
       />
-      <h1> {title} </h1>
+      <h1 onClick={()=>{console.log(title)}}> {title} </h1>
       <h4> {author} </h4>
+      <button type="button" onClick={clickHandler} > EXAMPLE</button>
+      <button type="button" onClick={()=> complexExample(author)} > MORE EXAMPLE</button>
     </article>
   );
 };
